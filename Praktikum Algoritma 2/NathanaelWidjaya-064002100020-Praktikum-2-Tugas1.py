2494967ec74a30d0e7c1ec3499de00c7d727982d
crusta <90506739+crusta@users.noreply.github.com>
2021-09-27 14:04:40 +0700

    Upload Praktikum Algoritma 2 tugas 1

---
+++ Praktikum Algoritma 2/NathanaelWidjaya-064002100020-Praktikum-2-Tugas1.py
@@ -0,0 +1,25 @@
+
+import math
+a = float(input("masukan nilai a :"))
+b = float(input("masukan nilai b :"))
+
+hasil = a+b
+print("penambahan: " ,hasil)
+
+hasil = (a-b)
+print("pengurangan: ",abs(a-b))
+
+hasil = a*b
+print("perkalian: ",hasil)
+
+hasil = a%b
+print("sisa bagi: ",hasil)
+
+hasil = a/b
+print("hasil pembagian: ",hasil)
+
+hasil = pow(a,b)
+print("hasil pangkat: ",hasil)
+
+hasil = math.log10(a)
+print("hasil log a: ",hasil)
