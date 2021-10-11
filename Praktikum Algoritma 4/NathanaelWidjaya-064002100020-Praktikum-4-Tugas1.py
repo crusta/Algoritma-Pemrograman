8d2dfa04cecaf35d3da8db9cb15706d4a48d6f23
crusta <90506739+crusta@users.noreply.github.com>
2021-10-11 13:31:21 +0700

    Upload Praktikum 4 Tugas 1

---
+++ Praktikum Algoritma 4/NathanaelWidjaya-064002100020-Praktikum-4-Tugas1.py
@@ -0,0 +1,12 @@
+#Nathanael Widjaya - 064002100020
+#------------------------------------
+
+baris = int(input("berapa baris ?: "))
+
+while baris > 0:
+    for i in range(baris):
+        print(baris, end="")
+    print()
+    
+    baris -= 1
+    
