a867b60f87864ba15b97e170a6cc17ffecd2a36d
crusta <90506739+crusta@users.noreply.github.com>
2021-11-08 14:39:32 +0700

    Upload Praktikum 7 Tugas 1

---
+++ Praktikum Algoritma 7/NathanaelWidjaya-064002100020-Praktikum-7-Tugas1.py
@@ -0,0 +1,31 @@
+Created on Mon Nov  8 14:13:30 2021
+
+@author: PC
+"""
+
+def cekBil(bil):
+    if bil == 1:
+        print(bil,"= prima")
+    elif bil == 2:
+        print(bil,"= prima")
+    else:
+        global x
+        for x in range(2,bil):
+            if bil%x==0:
+                stat = 0
+                break
+            else:
+                stat = 1
+        cekStat(stat)
+        
+def cekStat(stat):
+    if stat == 0:
+        print(bil,"= bukan prima")
+        print(f"{x} x {bil//x} = {bil}")
+    else:
+        print(bil,"= prima")
+        
+while True:
+    print()
+    bil = int(input("Masukan angka:"))
+    cekBil(bil)
